e2d2b993a47ea52bd19299df69e94f13917f9835
Borhan Uddin <borhan.ur@gmail.com>
2018-05-29 02:46:07 +0600

    Add new HTML

--- app/src/main/java/mgks/os/webview/MainActivity.java
+++ app/src/main/java/mgks/os/webview/MainActivity.java
@@ -35,7 +35,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.app.NotificationCompat;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
